84af777e2bc43ff781301f316515c8e159ad1639
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-05 16:09:38 -0500

    Finish fizzbuzz kata with all constraints

--- fizzbuzz/lib/fizz_buzz.rb
+++ fizzbuzz/lib/fizz_buzz.rb
@@ -1,20 +1,46 @@
+# Required because of 'No if-else' constraint
+  # require_relative 'fizz_buzz_engine'
+
+# Required because of 'No if' constraint
+require_relative 'fixnum_extensions'
+
 class FizzBuzz
+  using FixnumExtensions
   def numbers
     (1..100).to_a
   end
 
+# Original implentation of Fizz Buzz Kata
+  # def sequence
+  #   numbers.collect do |x|
+  #     if multiple_of(3, x) && multiple_of(5, x)
+  #       'FizzBuzz'
+  #     elsif multiple_of(3, x)
+  #       'Fizz'
+  #     elsif multiple_of(5, x)
+  #       'Buzz'
+  #     else
+  #       x
+  #     end
+  #   end
+  # end
+
+# No if-else contstraint on Fizz Buzz Kata
+  # def sequence
+  #   numbers.collect do |x|
+  #     fbe = FizzBuzzEngine.new(x)
+  #     fbe.value
+  #   end
+  # end
+
   def sequence
     numbers.collect do |x|
-      if multiple_of(3, x) && multiple_of(5, x)
-        'FizzBuzz'
-      elsif multiple_of(3, x)
-        'Fizz'
-      elsif multiple_of(5, x)
-        'Buzz'
-      else
-        x
-      end
+      x.fizz_buzz
     end
+  end
+
+  def transform(n)
+    sequence[n - 1]
   end
 
   private
@@ -23,6 +49,18 @@ class FizzBuzz
     x % n == 0
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
